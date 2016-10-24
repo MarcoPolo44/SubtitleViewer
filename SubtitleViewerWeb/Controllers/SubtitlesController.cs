9e6647a3b78e2c77f863c9eb40a92cb62224f028
Marco Politakis <marco.politakis@gmail.com>
2016-10-24 19:35:26 +1300

    Publised website
    Made a few changes so that the published website would function
    correctly.

--- SubtitleViewerWeb/Controllers/SubtitlesController.cs
+++ SubtitleViewerWeb/Controllers/SubtitlesController.cs
@@ -53,7 +53,7 @@ namespace SubtitleViewerWeb.Controllers
             {
                 // Upload file
                 var fileName = Path.GetFileName(upload.File.FileName);
-                var filePath = Path.Combine(Server.MapPath("~/App_Data/Uploads"), fileName);
+                var filePath = Path.Combine(Server.MapPath("~/Subtitles"), fileName);
                 upload.File.SaveAs(filePath);
 
                 // Parse file
@@ -80,7 +80,7 @@ namespace SubtitleViewerWeb.Controllers
             string result = null;
             var parser = new SubtitlesParser.Classes.Parsers.SubParser();
 
-            var filePath = Path.Combine(Server.MapPath("~/App_Data/Uploads"), fileName);
+            var filePath = Path.Combine(Server.MapPath("~/Subtitles"), fileName);
 
             using (var fileStream = System.IO.File.OpenRead(filePath))
             {
@@ -204,7 +204,7 @@ namespace SubtitleViewerWeb.Controllers
         {
             resultList = (ResultListModel)TempData["result"];
 
-            if (resultList == null)
+            if (resultList == null || resultList.Subtitles.Count() == 0)
             {
                 TempData["error"] = "Sorry, no results could be found.";
                 return RedirectToAction("Error");
@@ -233,13 +233,13 @@ namespace SubtitleViewerWeb.Controllers
 
                 // Download file
                 var osdb = Osdb.Login("OSTestUserAgentTemp");
-                osdb.DownloadSubtitleToPath(Server.MapPath("~/App_Data/Uploads"), selectedResult);
+                osdb.DownloadSubtitleToPath(Server.MapPath("~/Subtitles"), selectedResult);
 
                 // Parse file
                 error = ParseSubtitles(selectedResult.SubtitleFileName, model.Time, model.Style);
 
                 // Delete file off server
-                var filePath = Path.Combine(Server.MapPath("~/App_Data/Uploads"), selectedResult.SubtitleFileName);
+                var filePath = Path.Combine(Server.MapPath("~/Subtitles"), selectedResult.SubtitleFileName);
                 System.IO.File.Delete(filePath);
             }
 
